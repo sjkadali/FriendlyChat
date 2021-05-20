cef6a47a3f4d0f848f68e55cbdf37d1d4453cfdb
sjkadali <skadalikat@gmail.com>
2021-05-20 10:35:44 -0400

    login page setup

--- ig-react-app/src/pages/login.js
+++ ig-react-app/src/pages/login.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 
@@ -11,8 +11,25 @@ export default function login() {
     const [password, setPassword] = useState('');
 
     const [error, setError] = useState('');
+    const isInvalid = password === '' || emailAddress === '';
+
+    const handleLogin = () => {};
+
+    // eslint-disable-next-line no-undef
+    useEffect(() => {
+        document.title = 'Login - Instagram';
+    }, []);
     
     return(
-        <p>Login works! </p>
+        <div className="conatiner flex mx-auto max-w-screen-md items-center h-screen">
+            <div className="flex w-3/5">
+                <img src="/images/iphone-with-profile.jpg" alt="Iphone with profile" />
+            </div>
+            <div className="flex flex-col w-2/5">
+                <h1 className="flex justify-center w-full">
+                    <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+                </h1>
+            </div>
+        </div>
     )
 }
