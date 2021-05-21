870b995c6b621975fc62ca4db463559dab112a73
sjkadali <skadalikat@gmail.com>
2021-05-21 18:06:05 -0400

    not-found page setup

--- ig-react-app/src/App.js
+++ ig-react-app/src/App.js
@@ -5,6 +5,8 @@ import * as ROUTES from './constants/routes';
 
 const Login = lazy(() => import ('./pages/login'));
 const SignUp = lazy(() => import ('./pages/sign-up'));
+const NotFound = lazy(() => import ('./pages/not-found'));
+const Dashboard = lazy(() => import ('./pages/dashboard'));
 
 
 function App() {    
@@ -12,8 +14,10 @@ function App() {
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
       <Switch>
-        <Route path={ROUTES.LOGIN} component={Login} />
-        <Route path={ROUTES.SIGN_UP} component={SignUp} />
+        <Route path={ROUTES.DASHBOARD} component={Dashboard}  exact/>
+        <Route path={ROUTES.LOGIN} component={Login} exact/>
+        <Route path={ROUTES.SIGN_UP} component={SignUp} exact/>
+        <Route component={NotFound}  />
       </Switch>
       </Suspense>
     </Router>    
