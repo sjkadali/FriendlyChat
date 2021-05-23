0547ffdd05faf9f1f625c935cc48ce06f9bb48e4
sjkadali <skadalikat@gmail.com>
2021-05-23 12:04:33 -0400

    dashboard page setup

---
+++ ig-react-app/src/components/timeline.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Timeline() {
+    return (
+        <div>
+            <p>I am the Timeline!</p>
+        </div>
+    )
+}
--- ig-react-app/src/pages/dashboard.js
+++ ig-react-app/src/pages/dashboard.js
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
+import Header  from '../components/header';
+import Timeline  from '../components/timeline';
+import Sidebar  from '../components/sidebar';
+
 export default function Dashboard() {
+
+    useEffect(() => {
+        document.title = 'Instagram';
+    }, []);
+
     return(
-        <div>
-            <h1>Welcome !!!</h1>
+        <div className="bg-gray-background">
+            <Header />
+            <div className="grid">
+                <Timeline />
+                <Sidebar />
+            </div>           
         </div>
     )
 }
