dccbc65c3ab26fa080396ed321f900ce5a0f1cab
sjkadali <skadalikat@gmail.com>
2021-05-30 20:24:36 -0400

    post/index, timeline changes

--- ig-react-app/src/components/post/index.js
+++ ig-react-app/src/components/post/index.js
@@ -3,7 +3,7 @@ import Image from './image';
 export default function Post({content}) {
     return (
         <div>
-            {content.docId} {content.username}
+            {content.docId} {content.caption}
             <Image src={content.imageSrc} />
         </div>
     )
--- ig-react-app/src/components/timeline.js
+++ ig-react-app/src/components/timeline.js
@@ -2,7 +2,7 @@ import Skeleton from 'react-loading-skeleton'
 import LoggedInUserContext from '../context/logged-in-user'
 import usePhotos from '../hooks/use-photos';
 import { useContext } from 'react';
-import Post from '../components/post/index';
+import Post from './post/index';
 
 export default function Timeline() {
     const { user } = useContext(LoggedInUserContext);
@@ -11,8 +11,10 @@ export default function Timeline() {
         <div className="container col-span-2">
             {!photos ? (
                 <Skeleton count={4} width={640} height={500} className="mb-5" />
-            ) : (
+            ) : photos?.length > 0 ? (
                 photos.map((content) => <Post key={content.docId} content={content} />)
+            ): (
+                <p className="text-center text-2x1">Follow people to see photos</p>
             )}  
         </div>
     );
