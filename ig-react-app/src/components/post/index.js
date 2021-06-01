6fadb638b0a8a708b5f25721a543728ec86fafc9
sjkadali <skadalikat@gmail.com>
2021-06-01 17:20:07 -0400

    firebase changes, post/footer added

--- ig-react-app/src/components/post/index.js
+++ ig-react-app/src/components/post/index.js
@@ -2,14 +2,24 @@ import Image from './image';
 import { useRef } from 'react';
 import PropTypes from 'prop-types';
 import Header from './header';
+import Actions from './actions';
+import Footer from './footer';
 
 export default function Post({ content }) {
-    
+    const commentInput = useRef(null);
+    const handleFocus = () => commentInput.current.focus();
+    console.log('docId, content: '+ content.docId, content);
     return (
         <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
-            {content.docId} {content.caption}
             <Header username={content.username} />
-            <Image src={content.imageSrc} />
+            <Image src={content.imageSrc} caption={content.caption} />
+            <Actions
+                docId={content.docId}
+                totalLikes={content.likes.length}
+                likedPhoto={content.userLikedPhoto}
+                handleFocus={handleFocus}
+            />
+            <Footer caption={content.caption} username={content.username}/>
         </div>
     )
 }
--- ig-react-app/src/services/firebase.js
+++ ig-react-app/src/services/firebase.js
@@ -1,4 +1,4 @@
-import { firebase, fieldValue } from '../lib/firebase';
+import { firebase, FieldValue } from '../lib/firebase';
 
 export async function doesUsernameExist(username) {
     const result = await firebase
@@ -50,8 +50,8 @@ export async function getSuggestedProfiles(userId, following) {
   ) {
     return firebase.firestore().collection('users').doc(loggedInUserDocId).update({
       following: isFollowingProfile
-        ? fieldValue.arrayRemove(profileId)
-        : fieldValue.arrayUnion(profileId)
+        ? FieldValue.arrayRemove(profileId)
+        : FieldValue.arrayUnion(profileId)
     });
   }
 
@@ -62,8 +62,8 @@ export async function getSuggestedProfiles(userId, following) {
   ) {
     return firebase.firestore().collection('users').doc(profileDocId).update({
       followers: isFollowingProfile
-        ? fieldValue.arrayRemove(loggedInUserDocId)
-        : fieldValue.arrayUnion(loggedInUserDocId)
+        ? FieldValue.arrayRemove(loggedInUserDocId)
+        : FieldValue.arrayUnion(loggedInUserDocId)
     });
   }
 
@@ -92,4 +92,52 @@ export async function getSuggestedProfiles(userId, following) {
     );
 
     return photosWithUserDetails;
+  }
+
+  export async function getUserPhotosByUserId(userId) {
+    const result = await firebase
+      .firestore()
+      .collection('photos')
+      .where('userId', '==', userId)
+      .get();
+  
+    const photos = result.docs.map((photo) => ({
+      ...photo.data(),
+      docId: photo.id
+    }));
+    return photos;
+  }
+  
+  export async function isUserFollowingProfile(loggedInUserUsername, profileUserId) {
+    const result = await firebase
+      .firestore()
+      .collection('users')
+      .where('username', '==', loggedInUserUsername) // karl (active logged in user)
+      .where('following', 'array-contains', profileUserId)
+      .get();
+  
+    const [response = {}] = result.docs.map((item) => ({
+      ...item.data(),
+      docId: item.id
+    }));
+  
+    return response.userId;
+  }
+  
+  export async function toggleFollow(
+    isFollowingProfile,
+    activeUserDocId,
+    profileDocId,
+    profileUserId,
+    followingUserId
+  ) {
+    // 1st param: karl's doc id
+    // 2nd param: raphael's user id
+    // 3rd param: is the user following this profile? e.g. does karl follow raphael? (true/false)
+    await updateLoggedInUserFollowing(activeUserDocId, profileUserId, isFollowingProfile);
+  
+    // 1st param: karl's user id
+    // 2nd param: raphael's doc id
+    // 3rd param: is the user following this profile? e.g. does karl follow raphael? (true/false)
+    await updateFollowedUserFollowers(profileDocId, followingUserId, isFollowingProfile);
   }
