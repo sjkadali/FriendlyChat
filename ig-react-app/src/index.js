a565ceb83ff0ddfb976245614e3691a9893a28f1
sjkadali <skadalikat@gmail.com>
2021-06-09 10:07:42 -0400

    IsUserLoggedIn Route added

--- ig-react-app/src/index.js
+++ ig-react-app/src/index.js
@@ -5,9 +5,11 @@ import FirebaseContext from './context/firebase';
 import { firebase, FieldValue } from './lib/firebase';
 import './styles/App.css';
 
-ReactDOM.render( <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+ReactDOM.render( 
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
     <App />
-  </FirebaseContext.Provider>, document.getElementById('root'));
+  </FirebaseContext.Provider>, 
+  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
